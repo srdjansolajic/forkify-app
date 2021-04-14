d5e4de792ac56b3eec63dc15f487d6db015f763e
srdjansolajic <solajic.srdjan@gmail.com>
2021-04-14 11:27:23 +0200

    git testing

--- src/js/model.js
+++ src/js/model.js
@@ -36,8 +36,6 @@ export const loadRecipe = async function (id) {
     if (state.bookmarks.some(bookmark => bookmark.id === id))
       state.recipe.bookmarked = true;
     else state.recipe.bookmarked = false;
-
-    console.log(state.recipe);
   } catch (err) {
     console.log(`${err} 💥💥💥💥`);
     throw err;
